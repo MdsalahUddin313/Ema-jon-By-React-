cb278fd47c0c31307377e9c2ac56b0cfe02ea284
MdsalahUddin313 <nabidbhuia314@gmail.com>
2020-05-26 22:05:29 +0600

    --- Auto-Git Commit ---

---
+++ src/Com/Product_imformation/Description.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Description = () => {
+    return (
+        <div>
+           <h1>Description part</h1>            
+        </div>
+    );
+};
+
+export default Description;
