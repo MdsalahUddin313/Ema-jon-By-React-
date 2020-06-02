3e115eb9277606a7e884905012e912cbd2870a11
MdsalahUddin313 <nabidbhuia314@gmail.com>
2020-06-03 00:48:48 +0600

    --- Auto-Git Commit ---

--- src/Com/Product_imformation/Description.js
+++ src/Com/Product_imformation/Description.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import { useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 const Description = () => {
+     const {productKey}=useParams();
     return (
         <div>
            <h1>Description part</h1>            
