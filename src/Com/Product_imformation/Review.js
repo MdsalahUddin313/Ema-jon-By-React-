80d531917af100f479e289ba5098019a99cd09db
MdsalahUddin313 <nabidbhuia314@gmail.com>
2020-06-03 13:21:18 +0600

    --- Auto-Git Commit ---

--- src/Com/Product_imformation/Review.js
+++ src/Com/Product_imformation/Review.js
@@ -1,6 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useState } from 'react';
+import { getDatabaseCart } from '../../utilities/databaseManager';
+import fakeData from '../../fakeData';
 
 const Review = () => {
+
+    const  {cart,setCart}=useState([]);
+
+    useEffect(()=>{
+          const savedCart=getDatabaseCart();
+          const ProductKeys=Object.keys(savedCart);
+
+          const cartProducts=ProductKeys.map(key=>{
+                 const product=fakeData.find(pd=>pd.key===key);
+                 product.quantity=savedCart[key];
+                 return product;
+                    })
+          console.log(ProductKeys);
+    }
+    )
     return (
         <div>
                <h1>Riview part</h1>            
