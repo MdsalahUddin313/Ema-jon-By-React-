31df0970489a4c2f9ca4679d25da7d12e70e80ee
MdsalahUddin313 <nabidbhuia314@gmail.com>
2020-06-03 08:50:19 +0600

    --- Auto-Git Commit ---

--- src/Com/Product_imformation/Description.js
+++ src/Com/Product_imformation/Description.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { useState } from 'react';
 import { useParams } from 'react-router-dom';
+import fakeData from '../../fakeData';
 
 const Description = () => {
-     const {productKey}=useParams(); 
+     const {Description}=useParams(); 
+     const Product_details=fakeData.find(pd=>pd.key===Description);
+     console.log(Product_details);
     return (
         <div>
-           <h1>{productKey}Description part</h1>            
+           <h1>{Description}Description part</h1> 
+           <img src={Product_details.img} alt=""></img>
+    <h1>{Product_details.name}</h1>           
         </div>
     );
 };
