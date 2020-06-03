955c8a10a86efa536b06acd0da1f69b548f4e155
MdsalahUddin313 <nabidbhuia314@gmail.com>
2020-06-03 10:20:36 +0600

    --- Auto-Git Commit ---

--- src/Com/Product_imformation/Description.js
+++ src/Com/Product_imformation/Description.js
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import fakeData from '../../fakeData';
 import Product from '../Products/Product';
+import './Pdescription.css'
 
 const Description = () => {
      const {Description}=useParams(); 
@@ -11,6 +12,7 @@ const Description = () => {
     return (
         <div>
            <h1>Your Product </h1>
+           <img src={Product_details.img} alt=""></img>
            <Product 
            showButton={false}
            Product={Product_details}></Product>
